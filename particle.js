6441b828bf1fceb68c18772537b996da1ce48ded
Camron2889 <Camron2889@gmail.com>
2018-11-15 16:44:51 -0500

    Add clone() method

--- particle.js
+++ particle.js
@@ -14,7 +14,10 @@
     const proto = Particle.prototype;
     
     proto.clone = function() {
+        const copy = new Particle(this.position.x, this.position.y, this.position.z, this.radius);
+        copy.color.set(this.color.r, this.color.g, this.color.b, this.color.a);
         
+        return copy;
     };
     
     particlejs.Particle = Particle;
