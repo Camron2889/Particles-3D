200f005654d15f6a97802fed7674e312cd5c607d
Camron2889 <Camron2889@gmail.com>
2018-11-15 16:07:40 -0500

    Tidy vector3.js
    Change Vector3 to store RGBA values as separate data members instead of in a "pos" array
    
    Fix indentation and change line grouping

--- vector3.js
+++ vector3.js
@@ -7,70 +7,79 @@ this.particlejs = this.particlejs || {};
 
     //constructor
     const Vector3 = function(x = 0, y = 0, z = 0) {
-        this.pos = [x, y, z];
+        this.x = x;
+        this.y = y;
+        this.z = z;
     };
     
-    const proto = ctor.prototype;
+    const proto = Vector3.prototype;
     
     proto.rotateZ = function(t) {
-      const pos = this.pos;
-      const cosT = Math.cos(t);
-      const sinT = Math.sin(t);
-      const x1 = pos[0] * cosT - pos[1] * sinT;
-      const y1 = pos[0] * sinT + pos[1] * cosT;
-      pos[0] = x1;
-      pos[1] = y1;
-      return this;
+        const cosT = Math.cos(t);
+        const sinT = Math.sin(t);
+
+        const x1 = this.x * cosT - this.y * sinT;
+        const y1 = this.x * sinT + this.y * cosT;
+        this.x = x1;
+        this.y = y1;
+
+        return this;
     };
     
     proto.rotateY = function(t) {
-      const pos = this.pos;
-      const cosT = Math.cos(t);
-      const sinT = Math.sin(t);
-      const z1 = pos[2] * cosT - pos[0] * sinT;
-      const x1 = pos[2] * sinT + pos[0] * cosT;
-      pos[2] = z1;
-      pos[0] = x1;
-      return this;
+        const cosT = Math.cos(t);
+        const sinT = Math.sin(t);
+
+        const z1 = this.z * cosT - this.x * sinT;
+        const x1 = this.z * sinT + this.x * cosT;
+        this.z = z1;
+        this.x = x1;
+
+        return this;
     };
     
     proto.rotateX = function(t) {
-      const pos = this.pos;
-      const cosT = Math.cos(t);
-      const sinT = Math.sin(t);
-      const y1 = pos[1] * cosT - pos[2] * sinT;
-      const z1 = pos[1] * sinT + pos[2] * cosT;
-      pos[1] = y1;
-      pos[2] = z1;
-      return this;
+        const cosT = Math.cos(t);
+        const sinT = Math.sin(t);
+
+        const y1 = this.y * cosT - this.z * sinT;
+        const z1 = this.y * sinT + this.z * cosT;
+        this.y = y1;
+        this.z = z1;
+
+        return this;
     };
     
     proto.rotate = function(tx, ty, tz) {
-      if (tz) this.rotateZ(tz);
-      if (ty) this.rotateY(ty);
-      if (tx) this.rotateX(tx);
-      return this;
+        if (tz) this.rotateZ(tz);
+        if (ty) this.rotateY(ty);
+        if (tx) this.rotateX(tx);
+        
+        return this;
     };
     
     proto.clone = function(target) {
-      target = target || new ctor();
-      target.pos = this.pos.slice();
-      target.radius = this.radius;
-      return target;
+        target = target || new ctor();
+        target.pos = this.pos.slice();
+        target.radius = this.radius;
+        
+        return target;
     };
     
     proto.add = function(otherV3) {
-      this.pos[0] += otherV3.pos[0];
-      this.pos[1] += otherV3.pos[1];
-      this.pos[2] += otherV3.pos[2];
-      return this;
+        this.x += otherV3.x;
+        this.y += otherV3.y;
+        this.z += otherV3.z;
+        
+        return this;
     };
     
     proto.scale = function(s) {
-      this.pos[0] *= s;
-      this.pos[1] *= s;
-      this.pos[2] *= s;
-      return this;
+        this.x *= s;
+        this.y *= s;
+        this.z *= s;
+        
+        return this;
     };
     
     particlejs.Vector3 = Vector3;
